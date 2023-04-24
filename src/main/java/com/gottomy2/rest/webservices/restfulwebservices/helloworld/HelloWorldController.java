16e4299dc170298e527d37e30702832ea7ab230a
gottomy2 <igottomy@gmail.com>
2023-04-24 19:59:01 +0200

    update: quick reminder of path variables

--- src/main/java/com/gottomy2/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/gottomy2/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.gottomy2.rest.webservices.restfulwebservices.helloworld;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,7 +13,12 @@ public class HelloWorldController {
     }
 
     @GetMapping("/hello-world-bean")
-    public HelloWorldBean helloWorldBean(){
+    public HelloWorldBean helloWorldBean() {
         return new HelloWorldBean("Hello World");
+    }
+
+    @GetMapping("/hello-world/{name}")
+    public HelloWorldBean helloWorldBean(@PathVariable String name) {
+        return new HelloWorldBean(String.format("Hello %s!", name));
     }
 }
