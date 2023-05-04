615a12f3360e28db6feff56fea298e9a1e4398a6
gottomy2 <igottomy@gmail.com>
2023-05-04 20:17:51 +0200

    update: added simple internationalization

--- src/main/java/com/gottomy2/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/gottomy2/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,11 +1,21 @@
 package com.gottomy2.rest.webservices.restfulwebservices.helloworld;
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Locale;
+
 @RestController
 public class HelloWorldController {
+
+    private MessageSource messageSource;
+
+    public HelloWorldController(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
 
     @GetMapping("/hello-world")
     public String helloWorld() {
@@ -20,5 +30,11 @@ public class HelloWorldController {
     @GetMapping("/hello-world/{name}")
     public HelloWorldBean helloWorldBean(@PathVariable String name) {
         return new HelloWorldBean(String.format("Hello %s!", name));
+    }
+
+    @GetMapping("/hello-world-internationalized")
+    public String helloWorldInternationalized() {
+        Locale locale = LocaleContextHolder.getLocale();
+        return messageSource.getMessage("good.morning.message",null,"Default Message", locale);
     }
 }
